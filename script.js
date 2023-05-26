33a68f6223b62f8778d06d236145c24e1baaf3a3
ansheshwar <95951810+ansheshwar@users.noreply.github.com>
2023-05-26 21:24:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("welcome to melophile");
 
 //Intitialize the Variable
 let songIndex = 0;
-let audioElement = new Audio('songs/1.mp3');
+let audioElement = new Audio('1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
